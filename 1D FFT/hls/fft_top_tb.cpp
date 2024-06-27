a31575b6600cafbc6b502386bcfd9bee985069a1
Subhi <mohammed.abourdan5@gmail.com>
2024-06-27 15:28:01 -0400

    modified hls tb to test from sw fft not csv, same in host. SW EMU still needs the .so file. HW EMU works perfectly. HW is producing zeros

--- 1D FFT/hls/fft_top_tb.cpp
+++ 1D FFT/hls/fft_top_tb.cpp
@@ -20,23 +20,101 @@ using namespace std;
 
 
 
+/**
+ * Implementation of the 1D Cooley-Tukey FFT, the input should have a length of power of 2. Non Recursive Implementation.
+ * Implementation taken from https://cp-algorithms.com/algebra/fft.html (accessed on Jun 25).
+ * @param a  input vector
+ * @param invert 1 if inverse fft
+ */
+void fft(vector<cmpx_data_t> &a, bool invert) {
+
+
+    int n = a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+
+        if (i < j)
+            swap(a[i], a[j]);
+    }
+
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = - 2 * M_PI / len * (invert ? -1 : 1);
+        cmpx_data_t wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cmpx_data_t w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cmpx_data_t u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if (invert) {
+        for (cmpx_data_t & x : a)
+            x /= n;
+    }
+}
 
 
 
 /**
- * Function to split a string by delimiter and store in vector
+ * Produces FFT Testing Data and expected output
+ * @param input_data   a vector that will later contatins the input data, must be a power of 2 size
+ * @param output_data  a vector that will later contatins the results of applying fft on the input data, must match the size of input_data
+ * @param frequencies  a vector containing the frequencies to be included
+ * @param amplitudes   a vector containing the corresponding amplitudes, must match the size of frequencies
+ * @param time_range   it's first element contatins the timing of first measurment. the second the last measurment.
+ * @param invert       whether you want the output inversefft or fft
  */
-vector<string> split(const string &s, char delimiter) {
-    vector<string> tokens;
-    string token;
-    istringstream tokenStream(s);
+void fft_data_generator(vector<cmpx_data_t> &input_data, vector<cmpx_data_t> &output_data, vector<float> &frequencies, vector<float> &amplitudes, vector<float> &time_range, bool invert){
 
-    while (getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
+    // Fast Failing Assertions
+    if (input_data.size() != output_data.size()){
+        throw runtime_error("Input data vector length must match output data vector length");
     }
 
-    return tokens;
+    if (log2(double(input_data.size())) != int(log2(double(input_data.size())))){
+        throw runtime_error("Data vector length must be a power of 2");
+    }
+
+    if (frequencies.size() != amplitudes.size()){
+        throw runtime_error("frequencies vector size must match vector amplitudes");
+    }
+
+    if (time_range.size() != 2){
+        throw runtime_error("time_range vector must have only two elements, starting point and an ending point");
+    }
+
+
+    const int fft_data_size = input_data.size();
+    const int n = frequencies.size();
+
+    const float time_start = time_range[0];
+    const float time_end = time_range[1];
+    const float time_interval = (time_end - time_start) / fft_data_size;
+
+    // Fill input data 
+    for (int i = 0; i < fft_data_size; i++){
+        const float t = time_start + i * time_interval;
+
+        for (int j=0; j<n; j++){
+            input_data[i] += (amplitudes[j] * sinf(2 * M_PI * frequencies[j] * t));
+            output_data[i] += (amplitudes[j] * sinf(2 * M_PI * frequencies[j] * t));       // we this run the fft function on this as it works inplace
+        } 
+
+    }
+
+    // Produce the fft results
+    fft(output_data, invert);
 }
+
+
 
 
 /**
@@ -49,63 +127,49 @@ bool approx_equal(const cmpx_data_t &a, const cmpx_data_t &b, double tolerance) 
 
 
 
+///////  Testing Data Parameters
+float sampling_rate = 0.01;
+vector<float> frequencies = {1,4,7};
+vector<float> amplitudes = {3,1,0.5}; 
+vector<float> time_range = {0, sampling_rate * FFT_LENGTH};
+bool invert = 0;
+double tolerance = 1e-2;
+
+
 
 int main() {
 
-    //////////   LOAD DATA   ///////////
+    /////////////   Generate Testing Data using Pure software implementaion  //////////////
+    vector<cmpx_data_t> software_generated_input_data(FFT_LENGTH, 0);      // to include the input data
+    vector<cmpx_data_t> software_generated_ouput_data(FFT_LENGTH, 0);      // to include the expected output data
 
-    cmpx_data_t input_array[FFT_LENGTH];
-    cmpx_data_t expected_output_array[FFT_LENGTH];
-    cmpx_data_t actual_output_array[FFT_LENGTH];
-
-
-    ifstream file("ftt_tb_data.csv");                             //File generated with a python script
-
-    if (!file.is_open()) {
-        cerr << "Error opening file!" << endl;
-        return 1;
-    }
-
-    string line;
-
-    getline(file, line);                                          //Read header line; skip
-    for (int i = 0; i < FFT_LENGTH; ++i) {
-
-        if (!getline(file, line)) {
-            cerr << "Error reading line " << i + 2 << " from file!" << endl;
-            return 1;
-        }
-
-        // Split line into tokens
-        // The first two cols contain the real and imaginary parts of the x inputs
-        // The last two have the expected outpur
-        vector<string> tokens = split(line, ',');
-
-        input_array[i] = cmpx_data_t(stof(tokens[0]), stof(tokens[1]));
-        expected_output_array[i] = cmpx_data_t(stof(tokens[2]), stof(tokens[3]));
-    }
-
-    file.close();
-    cout << "Loaded data:" << endl;
+    fft_data_generator(software_generated_input_data, software_generated_ouput_data, frequencies, amplitudes, time_range, invert);
 
 
 
     /////////////////  Call FFT function   //////////////
+    cmpx_data_t hardware_output_data[FFT_LENGTH];
+    cmpx_data_t hardware_input_data[FFT_LENGTH];
+    for (int i =0; i<FFT_LENGTH; i++){
+        hardware_input_data[i] = software_generated_input_data[i];
+    }
+
     bool ovflo;
-    const bool FORWARD_FFT = true;                                   // Use true for forward FFT, false for inverse FFT
-    fft_top(FORWARD_FFT, input_array, actual_output_array, &ovflo);
+    const bool FORWARD_FFT = true;                                // Use true for forward FFT, false for inverse FFT
+
+    fft_top(FORWARD_FFT, hardware_input_data, hardware_output_data, &ovflo);
 
 
 
-    /////////////////  Compare Results   //////////////    
+
+    /////////////////  Validate Results   //////////////    
     bool passed = true;
-    double tolerance = 1e-3;
 
     for (int j = 0; j < FFT_LENGTH; j++) {
-        if (!approx_equal(expected_output_array[j], actual_output_array[j], tolerance)) {
+        if (!approx_equal(hardware_output_data[j], software_generated_ouput_data[j], tolerance)) {
             cerr << "Error at index " << j 
-                 << ": Expected (" << expected_output_array[j].real() << ", " << expected_output_array[j].imag() 
-                 << "), Actual ("  <<   actual_output_array[j].real() << ", " <<   actual_output_array[j].imag() 
+                 << ": Expected (" << software_generated_ouput_data[j].real() << ", " << software_generated_ouput_data[j].imag() 
+                 << "), Actual ("  <<   hardware_output_data[j].real() << ", " <<   hardware_output_data[j].imag() 
                  << ")" << endl;
 
             passed = false;
@@ -117,9 +181,10 @@ int main() {
         return 0;
     } else if (ovflo){
         cout << " (OVERFLOW!!!)" << endl;
-        return 0;
+        return 1;
     } else{
         cout << "Tests failed!" << endl;
         return 1;
     }
+
 }
--- 1D FFT/host/fft_app.cpp
+++ 1D FFT/host/fft_app.cpp
@@ -16,7 +16,9 @@
 #include <time.h>
 
 
+
 // XRT includes
+#include "xfft_v9_1_bitacc_cmodel.h"
 #include "xrt/xrt_bo.h"
 #include <experimental/xrt_xclbin.h>
 #include "xrt/xrt_device.h"
@@ -26,8 +28,6 @@
 using namespace std;
 
 
-const int  FFT_LENGTH = 2048; 
-const bool FORWARD_FFT = true;
 
 
 typedef complex<float> cmpx_data_t;
@@ -46,23 +46,100 @@ double get_time_stamp(){
 
 
 
-/**
- * Function to split a string by delimiter and store in vector
- */
-vector<string> split(const string &s, char delimiter) {
-    vector<string> tokens;
-    string token;
-    istringstream tokenStream(s);
 
-    while (getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
+/**
+ * Implementation of the 1D Cooley-Tukey FFT, the input should have a length of power of 2. Non Recursive Implementation.
+ * Implementation taken from https://cp-algorithms.com/algebra/fft.html (accessed on Jun 25).
+ * @param a  input vector
+ * @param invert 1 if inverse fft
+ */
+void fft(vector<cmpx_data_t> &a, bool invert) {
+
+
+    int n = a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+
+        if (i < j)
+            swap(a[i], a[j]);
     }
 
-    return tokens;
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = - 2 * M_PI / len * (invert ? -1 : 1);
+        cmpx_data_t wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cmpx_data_t w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cmpx_data_t u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if (invert) {
+        for (cmpx_data_t & x : a)
+            x /= n;
+    }
 }
 
 
 
+/**
+ * Produces FFT Testing Data and expected output
+ * @param input_data   a vector that will later contatins the input data, must be a power of 2 size
+ * @param output_data  a vector that will later contatins the results of applying fft on the input data, must match the size of input_data
+ * @param frequencies  a vector containing the frequencies to be included
+ * @param amplitudes   a vector containing the corresponding amplitudes, must match the size of frequencies
+ * @param time_range   it's first element contatins the timing of first measurment. the second the last measurment.
+ * @param invert       whether you want the output inversefft or fft
+ */
+void fft_data_generator(vector<cmpx_data_t> &input_data, vector<cmpx_data_t> &output_data, vector<float> &frequencies, vector<float> &amplitudes, vector<float> &time_range, bool invert){
+
+    // Fast Failing Assertions
+    if (input_data.size() != output_data.size()){
+        throw runtime_error("Input data vector length must match output data vector length");
+    }
+
+    if (log2(double(input_data.size())) != int(log2(double(input_data.size())))){
+        throw runtime_error("Data vector length must be a power of 2");
+    }
+
+    if (frequencies.size() != amplitudes.size()){
+        throw runtime_error("frequencies vector size must match vector amplitudes");
+    }
+
+    if (time_range.size() != 2){
+        throw runtime_error("time_range vector must have only two elements, starting point and an ending point");
+    }
+
+
+    const int fft_data_size = input_data.size();
+    const int n = frequencies.size();
+
+    const float time_start = time_range[0];
+    const float time_end = time_range[1];
+    const float time_interval = (time_end - time_start) / fft_data_size;
+
+    // Fill input data 
+    for (int i = 0; i < fft_data_size; i++){
+        const float t = time_start + i * time_interval;
+
+        for (int j=0; j<n; j++){
+            input_data[i] += (amplitudes[j] * sinf(2 * M_PI * frequencies[j] * t));
+            output_data[i] += (amplitudes[j] * sinf(2 * M_PI * frequencies[j] * t));       // we this run the fft function on this as it works inplace
+        } 
+
+    }
+
+    // Produce the fft results
+    fft(output_data, invert);
+}
 
 
 
@@ -78,7 +155,17 @@ bool approx_equal(const cmpx_data_t &a, const cmpx_data_t &b, double tolerance) 
 
 
 
-// timing variables
+///////  Testing Data Parameters
+const int  FFT_LENGTH = 2048; 
+const float sampling_rate = 0.01;
+vector<float> frequencies = {1,4,7};
+vector<float> amplitudes = {3,1,0.5}; 
+vector<float> time_range = {0, sampling_rate * FFT_LENGTH};
+bool invert = 0;
+double tolerance = 1e-2;
+
+
+/////// Timing Variables
 double hardware_start;
 double hardware_end;
 double hardware_time;
@@ -86,59 +173,33 @@ double hardware_time;
 
 int main(int argc, char ** argv) {
 
-
-    std::cout <<"argc = "<<argc <<std::endl;
-    std::cout <<argv[0] <<endl;
-    std::cout <<argv[1] <<endl;
+    cout << "argc = " << argc <<endl;
 
 
+    ////////////////              Generate Testing Data            /////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    cout << "Generating Testing Data..." << endl;
+    vector<cmpx_data_t> software_generated_input_data(FFT_LENGTH, 0);      // to include the input data
+    vector<cmpx_data_t> software_generated_ouput_data(FFT_LENGTH, 0);      // to include the expected output data
 
-    ////////////////////    LOAD DATA    /////////////////////
-    //////////////////////////////////////////////////////////
+    fft_data_generator(software_generated_input_data, software_generated_ouput_data, frequencies, amplitudes, time_range, invert);
 
-    cmpx_data_t input_array[FFT_LENGTH];
-    cmpx_data_t expected_output_array[FFT_LENGTH];
-    cmpx_data_t actual_output_array[FFT_LENGTH];
-    bool ovflo;
-
-    ifstream file("./ftt_tb_data.csv");   
-
-    if (!file.is_open()) {
-        cout << "Error opening file!" << endl;
-        return 1;
-    }
-
-    string line;
-    getline(file, line);                                    // Read header line
-
-    for (int i = 0; i < FFT_LENGTH; ++i) {
-        if (!getline(file, line)) {
-            cerr << "Error reading line " << i + 2 << " from file!" << endl;
-            return 1;
-        }
-
-        vector<string> tokens = split(line, ',');
-
-        input_array[i] = cmpx_data_t(stof(tokens[0]), stof(tokens[1]));
-        expected_output_array[i] = cmpx_data_t(stof(tokens[2]), stof(tokens[3]));
-    }
-
-    file.close();
-    cout << "Loaded data successfully" << endl;
-
+    cout << "Testing Data Generated" << endl;
 
 
     ////////////////     Initialize XRT device and load xclbin    //////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////
 
-    // std::string binaryFile = "./fft_top.xclbin";
-
     int device_index = 0;
-    std::string XCLBinaryFileName = argv[1];
+    std::string XCLBinaryFileName = argv[1];         // fft_top.xclbin
+
     std::cout << "Open the device " << device_index << std::endl;
     auto device = xrt::device(device_index);
+
     std::cout << "Load the xclbin " << XCLBinaryFileName << std::endl;
     auto uuid = device.load_xclbin(XCLBinaryFileName);
+
+
 
     ////////////////        Set Kernel and argument               //////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////
@@ -148,41 +209,33 @@ int main(int argc, char ** argv) {
     size_t input_size_bytes = sizeof(cmpx_data_t) * FFT_LENGTH;
 
     std::cout << "Allocate Buffer in Global Memory\n";
-    auto boDirection = xrt::bo(device, sizeof(bool), krnl.group_id(0));
+
     auto boIn = xrt::bo(device, input_size_bytes, krnl.group_id(1));
     auto boOut = xrt::bo(device, input_size_bytes, krnl.group_id(2));
-    auto boOvFlo = xrt::bo(device, sizeof(bool), krnl.group_id(3));
 
-
-    auto boDirection_map = boDirection.map<bool*>();
     auto boIn_map = boIn.map<cmpx_data_t*>();
     auto boOut_map = boOut.map<cmpx_data_t*>();
-    auto boOvFlo_map = boOvFlo.map<bool*>();
 
-    // fill(boDirection_map, boDirection_map + 1, 0);
-    // fill(boIn_map, boIn_map + FFT_LENGTH, 0);
-    // fill(boOut_map, boOut_map + FFT_LENGTH, 0);
-    // fill(boOvFlo_map, boOvFlo_map + 1, 0);
+    fill(boIn_map, boIn_map + FFT_LENGTH, 0);
+    fill(boOut_map, boOut_map + FFT_LENGTH, 0);
 
 
     // Fill the data in the buffers
-    *boDirection_map = FORWARD_FFT;
+    std::cout << "Filling Buffers with input data\n";
     for (int i = 0; i < FFT_LENGTH; i++) {
-        boIn_map[i] = input_array[i];
+        boIn_map[i] = software_generated_input_data[i];
     }
+    bool forward_fft = 0;
+    bool ovFlo;
 
-
-    // Synchronize buffer content with device side
     cout << "Synchronize input buffer data to device global memory\n";
-    boDirection.sync(XCL_BO_SYNC_BO_TO_DEVICE);
     boIn.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-
 
 
     // Execution of the kernel
     cout << "Execution of the kernel\n";
     hardware_start = get_time_stamp();
-    auto run = krnl(boDirection, boIn, boOut, boOvFlo);
+    auto run = krnl(forward_fft, boIn, boOut, ovFlo);
     run.wait();
     hardware_end = get_time_stamp();
     hardware_time = (hardware_end-hardware_start)/1000;
@@ -193,36 +246,29 @@ int main(int argc, char ** argv) {
     // Get the output;
     cout << "Get the output data from the device" << std::endl;
     boOut.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-    boOvFlo.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-
-    for (int i = 0; i < FFT_LENGTH; i++) {
-        actual_output_array[i] = boOut_map[i];
-    }
-    ovflo = *boOvFlo_map;
 
 
     // Validate Results
+    cout << "Validating Output results" << std::endl;
     bool passed = true;
-    double tolerance = 1e-3;
 
-    for (int j = 0; j < FFT_LENGTH; ++j) {
-        if (!approx_equal(expected_output_array[j], actual_output_array[j], tolerance)) {
-            cerr << "Error at index " << j << ": Expected (" << expected_output_array[j].real() << ", "
-                 << expected_output_array[j].imag() << "), Actual (" << actual_output_array[j].real() << ", "
-                 << actual_output_array[j].imag() << ")" << endl;
+    for (int j = 0; j < FFT_LENGTH; j++) {
+        if (!approx_equal(software_generated_ouput_data[j], boOut_map[j], tolerance)) {
+            cerr << "Error at index " << j 
+                 << ": Expected (" << software_generated_ouput_data[j].real() << ", "  << software_generated_ouput_data[j].imag()
+                 << "), Actual (" << boOut_map[j].real() << ", " << boOut_map[j].imag() 
+                 << ")" << endl;
             passed = false;
         }
     }
 
 
-
     if (passed) {
         cout << "All tests passed!" << endl;
-        return 0;
-    } else {
+    } else if (ovFlo){
+        cout << " (OVERFLOW!!!)" << endl;
+    } else{
         cout << "Tests failed!" << endl;
-        return 1;
     }
 
-    // ./fft_app fft_top.xclbin
 }
